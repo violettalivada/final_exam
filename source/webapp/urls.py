f2f5cf5e45559beaaf9be6ebd4f28794fbd44156
vilka <vilkalivada@gmail.com>
2021-01-23 20:47:38 +0600

    Добавлены фикстуры и архив с картинками

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('dialogs/', DialogsView.as_view(), name='dialogs'),
     path('messages/', MessagesView.as_view(), name='messages'),
-    path('<int:pk>/create/', CreateDialogView.as_view(), name='create_dialog'),
 ]
